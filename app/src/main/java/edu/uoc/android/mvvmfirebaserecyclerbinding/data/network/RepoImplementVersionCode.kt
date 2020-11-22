88188643149599d170b00e171e61e69938e2f16b
xexi81 <74619705+xexi81@users.noreply.github.com>
2020-11-22 17:36:58 +0100

    V1.2: Búsqueda síncrona de la versión

--- app/src/main/java/edu/uoc/android/mvvmfirebaserecyclerbinding/data/network/RepoImplementVersionCode.kt
+++ app/src/main/java/edu/uoc/android/mvvmfirebaserecyclerbinding/data/network/RepoImplementVersionCode.kt
@@ -2,9 +2,10 @@ package edu.uoc.android.mvvmfirebaserecyclerbinding.data.network
 
 import com.google.firebase.firestore.FirebaseFirestore
 import edu.uoc.android.mvvmfirebaserecyclerbinding.valueObject.Resource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 
 class RepoImplementVersionCode: IRepoVersionCode {
@@ -24,11 +25,27 @@ class RepoImplementVersionCode: IRepoVersionCode {
             return Resource.Success(versionCode.toInt())
     }
 
-    override suspend fun getVersionCodeFlow(): Flow<Resource<Int>> = flow {
-        emit(Resource.Success(2))
-        kotlinx.coroutines.delay(3000)
-        emit(Resource.Success(3))
-        kotlinx.coroutines.delay(3000)
-        emit(Resource.Success(4))
+    @ExperimentalCoroutinesApi
+    override suspend fun getVersionCodeFlow(): Flow<Resource<Int>> = callbackFlow {
+        val eventDocument = FirebaseFirestore
+            .getInstance()
+            .collection("Params")
+            .document("app")
+
+        val subscription = eventDocument.addSnapshotListener{ documentSnapshot, _ ->
+            if (documentSnapshot != null && documentSnapshot.exists()) {
+                if (documentSnapshot.getLong("version") == null) {
+                    val version = 0
+                    offer(Resource.Success(version))
+                } else {
+                    val version = documentSnapshot.getLong("version")!!.toInt()
+                    offer(Resource.Success(version))
+                }
+            } else {
+                channel.close()
+            }
+        }
+
+        awaitClose{ subscription.remove() }
     }
 }
